1607465733a65dd2652bab0cd7784344b1d4235b
superdjmarc <ma407g@gmail.com>
2020-09-01 20:06:09 +0100

    Create index.php
    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+if (!isset($myObj)) $myObj = new stdClass();
+	
+$myObj->result = (int)$_GET['fruit'];
+$myObj->age = 30;
+$myObj->city = "New York";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+	
+}
+?>
