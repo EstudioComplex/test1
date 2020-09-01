3beb33a27732c1b2b05b140aee01b2fdc34e7817
superdjmarc <ma407g@gmail.com>
2020-09-01 23:05:16 +0100

    Update index.php
    SECOND addition on the REMOTE

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
 ADDED THIS IN THE REMOTE
 if (!isset($myObj)) $myObj = new stdClass();
-	
+SECOND ADDITION IN THE REMOTE	
 $myObj->result = (int)$_GET['fruit'];
 $myObj->age = 30;
 $myObj->city = "New York";
